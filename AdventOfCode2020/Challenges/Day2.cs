2688b32022d53aec29d19c4b44bfa38352e5d805
thiseasm <44376778+thiseasm@users.noreply.github.com>
2020-12-02 20:49:01 +0200

    Finish Day 2

--- AdventOfCode2020/Challenges/Day2.cs
+++ AdventOfCode2020/Challenges/Day2.cs
@@ -1,23 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace AdventOfCode2020.Challenges
 {
     public class Day2 : Day
     {
-        private string[] _inputs;
+        private readonly string[] _inputs;
 
         public Day2()
         {
             _inputs = ReadFile("Day2.txt");
         }
 
+        [SuppressMessage("ReSharper", "LocalizableElement")]
         public override void Start()
         {
             var validPasswordsCount = ExecuteFirstPart(_inputs);
+            var validPasswordsUsingSecondRule = ExecuteSecondPart(_inputs);
 
             Console.WriteLine($"Valid Passwords: {validPasswordsCount}");
+            Console.WriteLine($"Valid Passwords using Second Rule: {validPasswordsUsingSecondRule}");
+        }
+
+        private static int ExecuteSecondPart(IEnumerable<string> inputs)
+        {
+            var validPasswordsFound = 0;
+            foreach (var compositePassword in inputs)
+            {
+                var ruleAndPassword = compositePassword.Split(": ");
+
+                var password = ruleAndPassword[1];
+                var positions = ruleAndPassword[0].Split(' ');
+                var firstPosition = int.Parse(positions[0].Split('-')[0]);
+                var secondPosition = int.Parse(positions[0].Split('-')[1]);
+                var targetLetter = char.Parse(positions[1]);
+
+                var firstPositionContainsLetter = password[firstPosition - 1].Equals(targetLetter);
+                var secondPositionContainsLetter = password[secondPosition - 1].Equals(targetLetter);
+
+                if (firstPositionContainsLetter ^ secondPositionContainsLetter)
+                {
+                    validPasswordsFound++;
+                }
+            }
+
+            return validPasswordsFound;
         }
 
         private static int ExecuteFirstPart(IEnumerable<string> inputs)
@@ -25,20 +54,21 @@ namespace AdventOfCode2020.Challenges
             var validPasswordsFound = 0;
             foreach (var compositePassword in inputs)
             {
-                var ruleAndPassword = compositePassword.Split(':');
+                var ruleAndPassword = compositePassword.Split(": ");
 
                 var password = ruleAndPassword[1];
                 var limits = ruleAndPassword[0].Split(' ');
                 var lowerLimit = int.Parse(limits[0].Split('-')[0]);
                 var upperLimit = int.Parse(limits[0].Split('-')[1]);
-                var letterRequired = limits[1];
+                var letterRequired = char.Parse(limits[1]);
 
-                var timesFound = password.Count(letter => letter.ToString().Equals(letterRequired));
+                var timesFound = password.Count(letter => letter.Equals(letterRequired));
                 if (timesFound >= lowerLimit && timesFound <= upperLimit)
                     validPasswordsFound++;
             }
 
             return validPasswordsFound;
         }
+
     }
 }
