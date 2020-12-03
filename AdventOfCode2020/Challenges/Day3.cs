39cc4b8f2f6e47396987179309585c72221290ee
thiseasm <44376778+thiseasm@users.noreply.github.com>
2020-12-03 21:21:49 +0200

    Finish Day 3

--- AdventOfCode2020/Challenges/Day3.cs
+++ AdventOfCode2020/Challenges/Day3.cs
@@ -18,11 +18,41 @@ namespace AdventOfCode2020.Challenges
         public override void Start()
         {
             var treesEncountered = CalculateTreesInPath(_inputs);
+            var treesOnCombinedSlopes = CalculateTreesInCombinedSlopes(_inputs);
 
             Console.WriteLine($"The sum of all trees encountered is:{treesEncountered}");
+            Console.WriteLine($"The product of all trees in slopes is:{treesOnCombinedSlopes}");
         }
 
         private static int CalculateTreesInPath(IEnumerable<string> inputs)
+        {
+            const int horizontalSpeed = 3;
+            const int verticalSpeed = 1;
+
+            return CalculateTreesInPath(inputs, horizontalSpeed, verticalSpeed);
+        }
+
+        private static object CalculateTreesInCombinedSlopes(string[] inputs)
+        {
+            var firstSlope = new[]{1,1};
+            var secondSlope = new[]{3,1};
+            var thirdSlope = new[]{5,1};
+            var fourthSlope = new[]{7,1};
+            var fifthSlope = new[]{1,2};
+
+            var slopeSpeeds = new List<int[]> {firstSlope,secondSlope,thirdSlope,fourthSlope,fifthSlope};
+            long productOfAllTreesEncountered = 1;
+
+            foreach (var speedPair in slopeSpeeds)
+            {
+                var treesInSlope = CalculateTreesInPath(inputs, speedPair[0], speedPair[1]);
+                productOfAllTreesEncountered *= treesInSlope;
+            }
+
+            return productOfAllTreesEncountered;
+        }
+
+        private static int CalculateTreesInPath(IEnumerable<string> inputs, int horizontalSpeed, int verticalSpeed)
         {
             var formattedMap = inputs.Select(row => row.ToCharArray()).ToArray();
 
@@ -32,8 +62,8 @@ namespace AdventOfCode2020.Challenges
 
             do
             {
-                xPosition += 3;
-                yPosition += 1;
+                xPosition += horizontalSpeed;
+                yPosition += verticalSpeed;
 
                 if (xPosition >= formattedMap[yPosition].Length)
                     xPosition -= formattedMap[yPosition].Length;
