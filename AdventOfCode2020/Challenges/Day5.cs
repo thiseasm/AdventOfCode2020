43e82c50cf6d617b0074930b5500f833cd3decb1
thiseasm <44376778+thiseasm@users.noreply.github.com>
2020-12-05 09:46:02 +0200

    Add Day 5 Part 1

---
+++ AdventOfCode2020/Challenges/Day5.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020.Challenges
+{
+    public class Day5 : Day
+    {
+        private readonly string[] _inputs;
+
+        public Day5()
+        {
+            _inputs = ReadFile("Day5.txt");
+        }
+        public override void Start()
+        {
+            var highestSeatId = GetHighestSeatId(_inputs);
+
+            Console.WriteLine($"The highest seat ID is: {highestSeatId}");
+        }
+
+        private int GetHighestSeatId(IEnumerable<string> inputs)
+        {
+            var highestId = 0;
+            foreach (var boardingPass in inputs)
+            {
+                var rowPosition = boardingPass.Substring(0, 7);
+                var columnPosition = boardingPass.Substring(7);
+
+                var rowNumber = GetRowNumber(rowPosition);
+                var columnNumber = GetColumnNumber(columnPosition);
+                var seatId = rowNumber * 8 + columnNumber;
+
+                if (seatId > highestId)
+                {
+                    highestId = seatId;
+                }
+            }
+
+            return highestId;
+        }
+
+        private static int GetRowNumber(string rowPosition)
+        {
+            var seatNumbers = new List<int>();
+            for (var index = 0; index < 128; index++)
+            {
+                seatNumbers.Add(index);
+            }
+
+            foreach (var character in rowPosition)
+            {
+                seatNumbers = character switch
+                {
+                    'F' => seatNumbers.GetRange(0, seatNumbers.Count / 2),
+                    'B' => seatNumbers.GetRange(seatNumbers.Count / 2, seatNumbers.Count / 2),
+                    _ => throw new IndexOutOfRangeException()
+                };
+            }
+
+            return seatNumbers[0];
+        }
+
+        private static int GetColumnNumber(string columnPosition)
+        {
+            var rowNumber = new List<int>();
+            for (var index = 0; index < 8; index++)
+            {
+                rowNumber.Add(index);
+            }
+
+            foreach (var character in columnPosition)
+            {
+                rowNumber = character switch
+                {
+                    'L' => rowNumber.GetRange(0, rowNumber.Count / 2),
+                    'R' => rowNumber.GetRange(rowNumber.Count / 2, rowNumber.Count / 2),
+                    _ => throw new IndexOutOfRangeException()
+                };
+            }
+
+            return rowNumber[0];
+        }
+
+        
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -19,13 +19,14 @@ namespace AdventOfCode2020
         }
         private static Day CalculateDay()
         {
-            const int date = 4;
+            const int date = 5;
             return date switch
             {
                 1 => (Day) new Day1(),
                 2 => (Day) new Day2(),
                 3 => (Day) new Day3(),
                 4 => (Day) new Day4(),
+                5 => (Day) new Day5(),
                 _ => new Day1()
             };
         }
