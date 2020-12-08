c08094f6c00ee183b2bd774c1eb8e88ab1124c28
thiseasm <44376778+thiseasm@users.noreply.github.com>
2020-12-08 22:39:18 +0200

    Finish Day 8

--- AdventOfCode2020/Challenges/Day8.cs
+++ AdventOfCode2020/Challenges/Day8.cs
@@ -17,15 +17,82 @@ namespace AdventOfCode2020.Challenges
         public override void Start()
         {
             var accumulatorValue = RunProgramOnce();
+            var fixedAccumulatorValue = RunAndRepairProgram();
 
-            Console.WriteLine($"{accumulatorValue}");
+            Console.WriteLine($"The accumulator value at the end of the first pass is: {accumulatorValue}");
+            Console.WriteLine($"The accumulator value of the fixed OS is: {fixedAccumulatorValue}");
+        }
+
+        private int RunAndRepairProgram()
+        {
+            var accumulatorValue = 0;
+            for (var counter = 0; counter < _inputs.Length; counter++)
+            {
+                var inputs = ReadFile("Day8.txt");
+                switch (_inputs[counter].Split(' ')[0])
+                {
+                    case "jmp":
+                        inputs[counter] = _inputs[counter].Replace("jmp", "nop");
+                        break;
+                    case "nop":
+                        inputs[counter] = _inputs[counter].Replace("nop", "nop");
+                        break;
+                    default:
+                        continue;
+                }
+
+                var completedSuccessfully = CanRunToCompletion(inputs, out accumulatorValue);
+
+                if (completedSuccessfully)
+                    break;
+            }
+
+
+            return accumulatorValue;
+        }
+
+        private static bool CanRunToCompletion(IReadOnlyList<string> inputs,out int accumulatorValue)
+        {
+            accumulatorValue = 0;
+            var executedCommands = new List<int>();
+            var index = 0;
+
+            do
+            {
+                var command = inputs[index].Split(' ');
+                executedCommands.Add(index);
+                switch (command[0])
+                {
+                    case "acc":
+                        var value = int.Parse(command[1]);
+                        accumulatorValue += value;
+                        index++;
+                        break;
+                    case "jmp":
+                        var step = int.Parse(command[1]);
+                        index += step;
+                        break;
+                    case "nop":
+                        index++;
+                        break;
+                }
+
+                if (index == inputs.Count)
+                {
+                    return true;
+                }
+
+            } while (!executedCommands.Contains(index));
+
+            accumulatorValue = 0;
+            return false;
         }
 
         private int RunProgramOnce()
         {
             var accumulatorValue = 0;
-            var executedCommands = new List<int>();
             var index = 0;
+            var executedCommands = new List<int>();
 
             do
             {
