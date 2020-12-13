2611fedf3c6cc3d928f31bf962a18a8afe6dde39
thiseasm <44376778+thiseasm@users.noreply.github.com>
2020-12-13 22:39:33 +0200

    Finish Day 12

---
+++ AdventOfCode2020/Helpers/Day12/PositionHelper.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Helpers.Day12
+{
+    public class PositionHelper
+    {
+        public Dictionary<Direction,int> Location { get; }
+        public Direction FacingDirection { get; protected set; }
+
+        public PositionHelper()
+        {
+            Location= new Dictionary<Direction, int>
+            {
+                {Direction.North, 0},
+                {Direction.East, 0},
+                {Direction.South, 0},
+                {Direction.West, 0}
+            };
+
+            FacingDirection = Direction.East;
+        }
+
+        public virtual void ExecuteAction(string action)
+        {
+            var (command, value) = ParseAction(action);
+
+            switch (command)
+            {
+                case 'F':
+                    MoveToDirection(FacingDirection.ToString()[0],value);
+                    break;
+                case 'L':
+                case 'R':
+                    ChangeFacingDirection(command,value);
+                    break;
+                default:
+                    MoveToDirection(command,value);
+                    break;
+            }
+        }
+
+        public virtual int GetCurrentPosition() => Location.Values.Sum();
+
+        protected virtual void ChangeFacingDirection(char direction, int degrees)
+        {
+            if (direction.Equals('R'))
+            {
+                while (degrees > 0)
+                {
+                    FacingDirection += 1;
+                    if ((int)FacingDirection > 3)
+                    {
+                        FacingDirection = 0;
+                    }
+
+                    degrees -= 90;
+                }
+            }
+            else
+            {
+                while (degrees > 0)
+                {
+                    FacingDirection -= 1;
+                    if ((int)FacingDirection < 0)
+                    {
+                        FacingDirection = (Direction)3;
+                    }
+
+                    degrees -= 90;
+                }
+            }
+        }
+
+        protected void MoveToDirection(char command, int units)
+        {
+            switch (command)
+            {
+                case 'N':
+                    MoveUsingNavigation(units,Direction.North,Direction.South);
+                    break;
+                case 'S':
+                    MoveUsingNavigation(units,Direction.South,Direction.North);
+                    break;
+                case 'E':
+                    MoveUsingNavigation(units,Direction.East,Direction.West);
+                    break;
+                case 'W':
+                    MoveUsingNavigation(units,Direction.West,Direction.East);
+                    break;
+            }
+        }
+
+        protected void MoveUsingNavigation(int units, Direction toDirection, Direction opposite)
+        {
+            if (Location[opposite] > 0)
+            {
+                Location[opposite] -= units;
+                if (Location[opposite] < 0)
+                {
+                    Location[toDirection] += -Location[opposite];
+                    Location[opposite] = 0;
+                }
+            }
+            else
+            {
+                Location[toDirection] += units;
+            }
+        }
+
+        protected static KeyValuePair<char,int> ParseAction(string action)
+        {
+            var command = action[0];
+            var units = int.Parse(action.Substring(1));
+
+            return new KeyValuePair<char, int>(command,units);
+        }
+    }
+
+}
---
+++ AdventOfCode2020/Helpers/Day12/WaypointHelper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Helpers.Day12
+{
+    public class WaypointHelper : PositionHelper
+    {
+        public Dictionary<Direction,int> ShipPosition { get; }
+
+        public WaypointHelper()
+        {
+            ShipPosition = new Dictionary<Direction, int>
+            {
+                {Direction.North, 0},
+                {Direction.East, 0},
+                {Direction.South, 0},
+                {Direction.West, 0}
+            };
+
+            Location[Direction.East] = 10;
+            Location[Direction.North] = 1;
+        }
+
+        public override void ExecuteAction(string action)
+        {
+            var (command, value) = ParseAction(action);
+
+            switch (command)
+            {
+                case 'F':
+                    MoveToWaypoint(value);
+                    break;
+                case 'L':
+                case 'R':
+                    ChangeFacingDirection(command,value);
+                    break;
+                default:
+                    MoveToDirection(command,value);
+                    break;
+            }
+        }
+
+        private void MoveToWaypoint(int timesToWaypoint)
+        {
+            foreach (var direction in Location.Keys)
+            {
+                var command = direction switch
+                {
+                    Direction.East => 'E',
+                    Direction.North => 'N',
+                    Direction.South => 'S',
+                    Direction.West => 'W',
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                var units = Location[direction] * timesToWaypoint;
+
+                switch (command)
+                {
+                    case 'N':
+                        MoveShip(units,Direction.North,Direction.South);
+                        break;
+                    case 'S':
+                        MoveShip(units,Direction.South,Direction.North);
+                        break;
+                    case 'E':
+                        MoveShip(units,Direction.East,Direction.West);
+                        break;
+                    case 'W':
+                        MoveShip(units,Direction.West,Direction.East);
+                        break;
+                }
+            }
+            
+        }
+        public override int GetCurrentPosition() =>  ShipPosition.Values.Sum();
+
+        protected void MoveShip(int units, Direction toDirection, Direction opposite)
+        {
+            if (ShipPosition[opposite] > 0)
+            {
+                ShipPosition[opposite] -= units;
+                if (ShipPosition[opposite] < 0)
+                {
+                    ShipPosition[toDirection] += -ShipPosition[opposite];
+                    ShipPosition[opposite] = 0;
+                }
+            }
+            else
+            {
+                ShipPosition[toDirection] += units;
+            }
+        }
+
+        protected override void ChangeFacingDirection(char direction, int degrees)
+        {
+            if (direction.Equals('R'))
+            {
+                while (degrees > 0)
+                {
+                    var keys = Location.Keys.ToArray();
+                    var values = Location.Values.ToArray();
+
+                    Location[keys[0]] = values[3];
+                    Location[keys[1]] = values[0];
+                    Location[keys[2]] = values[1];
+                    Location[keys[3]] = values[2];
+                    
+                    degrees -= 90;
+                }
+            }
+            else
+            {
+                while (degrees > 0)
+                {
+                    var keys = Location.Keys.ToArray();
+                    var values = Location.Values.ToArray();
+
+                    Location[keys[0]] = values[1];
+                    Location[keys[1]] = values[2];
+                    Location[keys[2]] = values[3];
+                    Location[keys[3]] = values[0];
+
+                    degrees -= 90;
+                }
+            }
+        }
+    }
+}
